b2d2cd284b633e0fd2fcff9c6963d192b425c8b5
Jacob J <jacobjpgit@yahoo.com>
2021-11-19 19:49:47 -0500

    Actually commits the files

--- src/main/java/Week1Challenge/Week1ChallengeMain.java
+++ src/main/java/Week1Challenge/Week1ChallengeMain.java
@@ -1,2 +1,24 @@
-package Week1Challenge;public class Week1ChallengeMain {
+package Week1Challenge;
+
+import java.util.Scanner;
+
+public class Week1ChallengeMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number to check for ascending");
+        String num = scanner.nextLine();
+        System.out.println("Ascending number = " + Week1Solution.ascending(num));
+
+        System.out.println("Enter number to create patch");
+        num = scanner.nextLine();
+        int[][] square = Week1Solution.squarePatch(num);
+        for (int[] row : square) {
+            for (int point : row) {
+                System.out.print(point + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/test/java/Week1Challenge/Week1SolutionTesting.java
+++ src/test/java/Week1Challenge/Week1SolutionTesting.java
@@ -1,5 +1,52 @@
 package Week1Challenge;
 
-public class Week1ChallengeTesting {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Week1SolutionTesting {
+
+    private Week1Solution week1SolutionTest;
+
+    @BeforeEach
+    public void init() {
+        week1SolutionTest = new Week1Solution();
+    }
+
+    @Test
+    public void testAscendingSuccess1() {
+        String testString = "232425";
+        assertTrue(week1SolutionTest.ascending(testString));
+    }
+
+    @Test
+    public void testAscendingSuccess2() {
+        String testString = "444445";
+        assertTrue(week1SolutionTest.ascending(testString));
+    }
+
+    @Test
+    public void testAscendingFailBadNum() {
+        String testString = "2324256";
+        assertFalse(week1SolutionTest.ascending(testString));
+    }
+
+    @Test
+    public void testAscendingFailBadString() {
+        String testString = "abc";
+        assertFalse(week1SolutionTest.ascending(testString));
+    }
+
+    @Test
+    public void testSquarePatch() {
+        int[][] correctArray = {{3, 3, 3},
+                                {3, 3, 3},
+                                {3, 3, 3,}};
+        int[][] testArray = week1SolutionTest.squarePatch(3);
+        assertTrue(Arrays.deepEquals(correctArray, testArray));
+    }
 }
